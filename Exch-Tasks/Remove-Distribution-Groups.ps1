a5a27d880c3cae5614314a15e9948400c3177aeb
Philip Kealley <pkealley@gmail.com>
2018-02-06 14:10:12 +0800

    Folder Structure
    Redesign Structure for script locations.

---
+++ Exch-Tasks/Remove-Distribution-Groups.ps1
@@ -0,0 +1,11 @@
+﻿$users = $args
+
+foreach ($user in $users)
+{
+    $groups = ((Get-ADPrincipalGroupMembership $user).where({$_.GroupCategory -EQ 'Distribution'}) | Select-Object -Property Name)
+   
+    foreach ($group in $groups) 
+    { 
+        Remove-ADPrincipalGroupMembership -Identity $username -MemberOf $group.name
+    }
+}
